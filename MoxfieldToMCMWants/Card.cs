2f686abc9b00906c4801797811f594ddd3593ed1
KarmaKamikaze <nicolaijgn@gmail.com>
2023-05-24 19:14:13 +0200

    Reorganize project folder

---
+++ MoxfieldToMCMWants/Card.cs
@@ -0,0 +1,53 @@
+﻿namespace MoxfieldToMCMWants;
+
+public class Card
+{
+    public Card(string name)
+    {
+        Name = name;
+        Set = null;
+        Language = null;
+        Condition = null;
+        Foil = null;
+        Signed = null;
+        Altered = null;
+        BuyPrice = null;
+        EmailAlarm = false;
+    }
+
+    public string Name { get; set; }
+    public string? Set { get; set; }
+    public string? Language { get; set; }
+    public string? Condition { get; set; }
+    public bool? Foil { get; set; }
+    public bool? Signed { get; set; }
+    public bool? Altered { get; set; }
+    public double? BuyPrice { get; set; }
+    public bool EmailAlarm { get; set; }
+
+    public enum Languages
+    {
+        English,
+        French,
+        German,
+        Spanish,
+        Italian,
+        SimplifiedChinese,
+        Japanese,
+        Portuguese,
+        Russian,
+        Korean,
+        TraditionalChinese
+    }
+
+    public enum Conditions
+    {
+        Mint,
+        NearMint,
+        Excellent,
+        Good,
+        LightPlayed,
+        Played,
+        Poor
+    }
+}
---
+++ MoxfieldToMCMWants/Deck.cs
@@ -0,0 +1,15 @@
+﻿namespace MoxfieldToMCMWants;
+
+public class Deck
+{
+    public Deck(string name, string url)
+    {
+        Name = name;
+        Url = url;
+        DeckList = new Dictionary<Card, int>();
+    }
+
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public Dictionary<Card, int> DeckList { get; set; }
+}
