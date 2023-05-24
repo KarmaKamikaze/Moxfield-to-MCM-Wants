b9dfb9edef04de0022c4caace0fe12f85b0e47a4
KarmaKamikaze <nicolaijgn@gmail.com>
2023-05-24 23:17:49 +0200

    FindProduct API call. Abandoned for now since API access is for Pro sellers.

---
+++ MoxfieldToMCMWants/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MoxfieldToMCMWants.Entities;
+
+public class Product
+{
+    public int IdProduct { get; set; }
+    public int IdMetaProduct { get; set; }
+    public int CountReprints { get; set; }
+    public string? EnName { get; set; }
+    public string? Website { get; set; }
+    public int Number { get; set; }
+    public string? Rarity { get; set; }
+    public string? Expansion { get; set; }
+}
