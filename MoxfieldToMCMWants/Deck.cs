b86e32b6439badbd2b896bfad3618d715fba4eba
KarmaKamikaze <nicolaijgn@gmail.com>
2023-05-24 22:29:04 +0200

    Deck export from Moxfield has been implemented

--- MoxfieldToMCMWants/Deck.cs
+++ MoxfieldToMCMWants/Deck.cs
@@ -12,4 +12,45 @@ public class Deck
     public string Name { get; set; }
     public string Url { get; set; }
     public Dictionary<Card, int> DeckList { get; set; }
+
+    public void ConstructDeckList(string deckList, bool keepEditions)
+    {
+        Dictionary<Card, int> deck = new Dictionary<Card, int>();
+
+        // Card example: 3 Island (3ed) 295
+        string[] cards = deckList.Split('\n');
+        foreach (string card in cards)
+        {
+            if(card.Contains("SIDEBOARD:")) // Skip sideboard cards
+                break;
+
+            List<string> fields = card.Split(' ').ToList();
+            if (!Int32.TryParse(fields[0], out int amount))
+                break;
+
+            string cardName = string.Empty;
+            for (int i = 1; i < fields.Count; i++) // skip first field since it is a number
+            {
+                if (fields[i].Contains('(') || fields[i].Contains(')')) // Stop when seeing edition marker
+                    break;
+                if (i == 1) // No space at the beginning of the card name
+                    cardName += fields[i];
+                else
+                    cardName += $" {fields[i]}";
+            }
+            Card newCard = new Card(cardName);
+
+            if (keepEditions)
+            {
+                string edition = fields.Find(s => s.Contains('('))!
+                    .Replace("(", "")
+                    .Replace(")", "");
+                newCard.Set = edition;
+            }
+
+            deck.Add(newCard, amount);
+        }
+
+        DeckList = deck;
+    }
 }
--- MoxfieldToMCMWants/Program.cs
+++ MoxfieldToMCMWants/Program.cs
@@ -1,4 +1,28 @@
 ﻿using MoxfieldToMCMWants;
 
+Console.WriteLine("Please insert the URL to a Moxfield deck.");
+string? url = Console.ReadLine();
+
+// Check if URL matches the http or https URI schemes
+if (!(Uri.TryCreate(url, UriKind.Absolute, out var uri) &&
+      (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)))
+    throw new ArgumentException("Not a valid URL.");
+
+Console.WriteLine("Would you like to retain the exact card editions? (y/n)");
+string? exactEditionsInput = Console.ReadLine();
+if (exactEditionsInput!.ToLower()[0] != 'y' && exactEditionsInput.ToLower()[0] != 'n')
+    throw new ArgumentException("Answer was not 'y' or 'n'.");
+bool exactEditions = exactEditionsInput[0] == 'y';
+
+Console.WriteLine("Exporting Moxfield deck list...");
 Settings config = new Settings();
 MoxfieldDeckGrabber deckGrabber = new MoxfieldDeckGrabber(config);
+Deck deck = deckGrabber.ExportDeck(url, exactEditions);
+deckGrabber.QuitDriver();
+
+
+Console.WriteLine(deck.Name);
+foreach (KeyValuePair<Card,int> card in deck.DeckList)
+{
+    Console.WriteLine($"{card.Value} {card.Key.Name}");
+}
